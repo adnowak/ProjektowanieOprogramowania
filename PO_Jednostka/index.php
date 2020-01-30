c8e51c1021c8ea63517bd3f1fd07ef10dbb92490
Alex Banert <alex@Alexs-Macbook.local>
2020-01-30 12:40:22 +0100

    changes

--- PO_Jednostka/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include 'service.php'; 
-    include 'controller.php'; 
-    include 'router.php';
-    $service = new Service();
-    new Controller($service);
-    new Router($service);
-?>
--- PO_Jednostka/src/app/Model/Entities/Unit.php
+++ PO_Jednostka/src/app/Model/Entities/Unit.php
@@ -13,33 +13,6 @@ class Unit extends Entity
         $this->initializeDB();
     }
 
-    public function casesInInterval($idUnit, $interval, $cases, $packages){
-        // TODO add implementation
-        if($interval = 1)
-            $epoch = 900;
-        elseif($interval = 2)
-            $epoch = 3600;
-        elseif($interval = 3)
-            $epoch = 86400;
-        elseif($interval = 4)
-            $epoch = 604800;
-        
-        foreach($cases as $case){
-            $packageId = $case['IdPackage'];
-            $unit = $case['IdUnit'];
-
-            $package = null;
-            foreach($packages as $packageIter){
-                if($packageIter['Id'] == $packageId){
-                    $package = $packageIter;
-                }
-            }
-        }
-        return 5;
-
-
-    }
-
     public function getId() 
     {
         $this->getData(self::ID);
